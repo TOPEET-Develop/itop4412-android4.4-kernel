e0c2b24a2893c033bd1e5c3db34c56c546f270c5
root <root@ubuntu.(none)>
2015-04-25 15:52:05 +0800

    test test

--- iTop4412_Kernel_3.0/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 80 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#80 SMP PREEMPT Thu Apr 23 16:21:52 CST 2015"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
